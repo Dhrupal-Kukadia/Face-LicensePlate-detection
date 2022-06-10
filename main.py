e409315b630a6d01ff36e9c4574323fa31c2d87f
ankitoscar <ankitoscar911@gmail.com>
2022-06-10 21:36:25 +0530

    Added functions for resizing and restoring cropping for bounding box coordinates

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+from this import s
 import darknet.darknet as dn # importing darknet wrapper for python
 import cv2
 import json
 import os
+import numpy as np
 
 import string
 import random
 
-
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
 
 VEHICLE_DETECTOR_CFG = "data/vehicle-detection.cfg"
 VEHICLE_DETECTOR_DATA = "data/vehicle-detection.data"
@@ -22,12 +21,21 @@ LP_DETECTOR_NAMES = "data/lp-detection-layout-classification.names"
 LP_DETECTOR_WEIGHTS = "data/lp-detection-layout-classification.weights"
 LP_THRESHOLD = 0.01
 
-def prepare_image(network, image_path, bounding_box=None, save_path = None):
+vehicle_network, vehicle_class_names, class_colors = dn.load_network(VEHICLE_DETECTOR_CFG, VEHICLE_DETECTOR_DATA, VEHICLE_DETECTOR_WEIGHTS)
+vehicle_width, vehicle_height = dn.network_width(vehicle_network), dn.network_height(vehicle_network)
+vehicle_network_shape = (vehicle_height, vehicle_width)
+
+lp_network, lp_class_names, class_colors = dn.load_network(LP_DETECTOR_CFG, LP_DETECTOR_DATA, LP_DETECTOR_WEIGHTS)
+lp_width, lp_height = dn.network_width(lp_network), dn.network_height(lp_network)
+lp_network_shape = (lp_height, lp_width)
+
+def prepare_image(network, image_path):
     width = dn.network_width(network)
     height = dn.network_height(network)
     darknet_image = dn.make_image(width, height, 3)
 
     image = cv2.imread(image_path)
+    image_shape = image.shape[:2]
 
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image_resized = cv2.resize(image_rgb, (width, height),
@@ -35,7 +43,7 @@ def prepare_image(network, image_path, bounding_box=None, save_path = None):
 
     dn.copy_image_from_bytes(darknet_image, image_resized.tobytes())
 
-    return darknet_image
+    return darknet_image, image_shape
 
 def save_image(image_path, save_path, image_name, box= None):
     if save_path:
@@ -45,125 +53,120 @@ def save_image(image_path, save_path, image_name, box= None):
         image = cv2.imread(image_path)
 
         if box:
+            box = [int(item) for item in box]
             image = image[box[1]: box[3], box[0]: box[2]]
 
         cv2.imwrite(os.path.join(save_path, image_name), image)
 
+def scaling(image_shape, network_shape, box):
+    scale = np.divide(image_shape, network_shape)
+
+    box = list(box)
+
+    box[0] = scale[1] * box[0]
+    box[1] = scale[0] * box[1]
+    box[2] = scale[1] * box[2]
+    box[3] = scale[0] * box[3]
+
+    return box
+
+def reset_crop(crop_location, bounding_box):
+    bounding_box[0] = crop_location[0] + bounding_box[0]
+    bounding_box[1] = crop_location[1] + bounding_box[1]
+    bounding_box[2] = crop_location[0] + bounding_box[2]
+    bounding_box[3] = crop_location[1] + bounding_box[3]
+
+    return bounding_box
 
 def vehicle_detector(image_path):
-    try:
-        network, class_names, class_colors = dn.load_network(VEHICLE_DETECTOR_CFG, VEHICLE_DETECTOR_DATA, VEHICLE_DETECTOR_WEIGHTS)
-        width, height = dn.network_width(network), dn.network_height(network)
-        print(width, height)
-        image = prepare_image(network, image_path)
-        detections = dn.detect_image(network, class_names, image, thresh=VEHICLE_DETECTOR_THRESHOLD)
+    image, image_shape = prepare_image(vehicle_network, image_path)
+    detections = dn.detect_image(vehicle_network, vehicle_class_names, image, thresh=VEHICLE_DETECTOR_THRESHOLD)
 
-        if(len(detections)):
-            result = {}
+    if(len(detections)):
+        bounding_boxes = [dn.bbox2points(item[2]) for item in detections]
 
-            image_name = image_path.split('/')[-1]
-            result_name = os.path.splitext(image_name)[0] + ".json"
+        scaled_bounding_boxes = [scaling(image_shape, vehicle_network_shape, box) for box in bounding_boxes]
 
-            filename = os.path.join("data/vehicle-detections", result_name)
+        result = {}
 
-            with open(filename, mode='w', encoding='utf-8') as f:
-                pass
-            
-            with open(filename, mode="a", encoding="utf-8") as f:
-                for i in range(len(detections)):
-                    item = {
-                        'vehicle': detections[i][0],
-                        'confidence': detections[i][1],
-                        'bounding box': list(detections[i][2]),
-                    }
+        image_name = image_path.split('/')[-1]
+        result_name = os.path.splitext(image_name)[0] + ".json"
 
-                    result[i] = item 
+        filename = os.path.join("data/vehicle-detections", result_name)
 
-                f.write(json.dumps(result))
-                f.close()
-
-            bounding_boxes = [dn.bbox2points(item[2]) for item in detections]
-
-            print("Found " + str(len(detections)) + " in the image.")
-            
-            vehicles = []
-
-            for i in range(len(bounding_boxes)):
-                vehicle_name = os.path.splitext(image_name)[0] + "_" + str(i) +".png"
-                vehicles.append(os.path.join("data/vehicle-detections/images", vehicle_name))
-                save_image(image_path, save_path="data/vehicle-detections/images", image_name=vehicle_name, box = bounding_boxes[i])
-
-            image_copy = cv2.imread(image_path)
-
-            image_copy = cv2.resize(image_copy, (width, height))
-
-            image_copy = dn.draw_boxes(detections, image_copy, class_colors)
-
-            cv2.imshow("Image", image_copy)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-            
-            return vehicles, result_name
+        with open(filename, mode='w', encoding='utf-8') as f:
+            pass
         
-        else:
-            print("No vehicles found")
+        with open(filename, mode="a", encoding="utf-8") as f:
+            for i in range(len(detections)):
+                item = {
+                    'vehicle': detections[i][0],
+                    'confidence': detections[i][1],
+                    'bounding box': scaled_bounding_boxes[i],
+                }
 
-    except Exception as e:
-        print(e)
+                result[i] = item 
 
-def lp_detector(vehicles, result_name):
-    try:
-        if(len(vehicles)):
-            network, class_names, class_colors = dn.load_network(LP_DETECTOR_CFG, LP_DETECTOR_DATA, LP_DETECTOR_WEIGHTS)
-            lp_width, lp_height = dn.network_width(network), dn.network_height(network)
-            print(lp_width, lp_height)
-            result = {}
+            f.write(json.dumps(result))
+            f.close()
 
-            for i in range(len(vehicles)):
-                lp_image = prepare_image(network, vehicles[i])
-                lp_detections = dn.detect_image(network, class_names, lp_image, thresh=LP_THRESHOLD)
+        print("Found " + str(len(detections)) + " in the image.")
+        
+        vehicles = []
 
-                if(len(lp_detections)):
-                    filename = os.path.join("data/lp-detections", result_name)
+        for i in range(len(scaled_bounding_boxes)):
+            vehicle_name = os.path.splitext(image_name)[0] + "_" + str(i) +".png"
+            vehicles.append(os.path.join("data/vehicle-detections/images", vehicle_name))
+            save_image(image_path, save_path="data/vehicle-detections/images", image_name=vehicle_name, box = scaled_bounding_boxes[i])
+        
+        return vehicles, result_name, scaled_bounding_boxes
+    
+    else:
+        print("No vehicles found")
 
-                    with open(filename, mode="w", encoding="utf-8") as f:
-                        pass
+def lp_detector(vehicles, result_name, crop_locations):
+    if(len(vehicles)):
+        result = {}
 
-                    with open(filename, mode="a", encoding="utf-8") as f:
-                        vehicle = {}
-                        for j in range(len(lp_detections)):
-                            lp = {
-                            'confidence': lp_detections[j][1],
-                            'bounding box': list(lp_detections[j][2]),
-                            }  
+        for i in range(len(vehicles)):
+            lp_image, lp_image_shape = prepare_image(lp_network, vehicles[i])
+            lp_detections = dn.detect_image(lp_network, lp_class_names, lp_image, thresh=LP_THRESHOLD)
 
-                            vehicle[j] = lp
-                        
-                        result[i] = vehicle
+            if(len(lp_detections)):
+                bounding_boxes = [dn.bbox2points(item[2]) for item in lp_detections]
 
-                        f.write(json.dumps(result))
-                        f.close()
+                scaled_bounding_boxes = [scaling(lp_image_shape, lp_network_shape, box) for box in bounding_boxes]
+
+                original_bounding_boxes = [reset_crop(crop_locations[i], box) for box in scaled_bounding_boxes]
+
+
+                filename = os.path.join("data/lp-detections", result_name)
+
+                with open(filename, mode="w", encoding="utf-8") as f:
+                    pass
+
+                with open(filename, mode="a", encoding="utf-8") as f:
+                    vehicle = {}
+                    for j in range(len(lp_detections)):
+                        lp = {
+                        'confidence': lp_detections[j][1],
+                        'bounding box': original_bounding_boxes[j],
+                        }  
+
+                        vehicle[j] = lp
                     
-                    print(str(len(lp_detections)) + " license plates were found.")
+                    result[i] = vehicle
 
-                    lp_image_copy = cv2.imread(vehicles[i])
-
-                    lp_image_copy = cv2.resize(lp_image_copy, (lp_width, lp_height))
-
-                    lp_image_copy = dn.draw_boxes(lp_detections, lp_image_copy, class_colors)
-
-                    cv2.imshow("Image", lp_image_copy)
-                    cv2.waitKey(0)
-                    cv2.destroyAllWindows()
+                    f.write(json.dumps(result))
+                    f.close()
                 
-                else:
-                    print("No license plates were found.")
-
-    except Exception as e:
-        print(e)
+                print(str(len(lp_detections)) + " license plates were found.")
+            
+            else:
+                print("No license plates were found.")
 
 def detect(image_path):
-    vehicles, result_name = vehicle_detector(image_path)
-    lp_detector(vehicles, result_name)
+    vehicles, result_name, crop_locations = vehicle_detector(image_path)
+    lp_detector(vehicles, result_name, crop_locations)
 
-detect("data/sample-image.jpg")
+detect("data/image.png")
